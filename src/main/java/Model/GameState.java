5a635e73816eb519978455970a8ac40c020a4872
EgyedAnna <78791535+EgyedAnna@users.noreply.github.com>
2021-05-07 08:53:59 +0200

    Added generateSnake() & SaveHead to GameState.java

--- src/main/java/Model/GameState.java
+++ src/main/java/Model/GameState.java
@@ -30,6 +30,7 @@ public class GameState {
                 gameState[i][j] = 0;
             }
         }
+        generateSnake();
     }
     public void setGameState(int[][] gameState) {
         this.gameState = gameState;
@@ -41,5 +42,19 @@ public class GameState {
 
     public boolean isOver() {
         return this.isOver;
+    }
+
+    public void generateSnake() {
+        int randomI = rand.nextInt(17);
+        int randomJ = rand.nextInt(20);
+        saveHead(randomI, randomJ);
+        gameState[randomI][randomJ] = 3;
+        gameState[++randomI][randomJ] = 2;
+        gameState[++randomI][randomJ] = 1;
+    }
+
+    public void saveHead(int headI, int headJ) {
+        this.head[0] = headI;
+        this.head[1] = headJ;
     }
 }
