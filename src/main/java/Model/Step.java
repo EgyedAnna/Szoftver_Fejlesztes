aeaeb767c74a69748fd5c66bf8241d80457c45bf
EgyedAnna <78791535+EgyedAnna@users.noreply.github.com>
2021-05-07 13:51:11 +0200

    Added checkIfNotSelf() & checkIfCellNotOver() to Step.java

--- src/main/java/Model/Step.java
+++ src/main/java/Model/Step.java
@@ -10,4 +10,19 @@ public class Step {
         this.col = col;
         this.snakeLength = snakeLength;
     }
+
+    public boolean checkIfNotSelf(int[][] gameState){
+        if(gameState[row][col]==0 || gameState[row][col]==-1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkIfCellNotOver(){
+        if(row>=0 && row<20 && col >=0 && col<26 ){
+            return true;
+        }
+        return false;
+    }
+
 }
